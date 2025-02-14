ae557104b6d9630b016f98951b228094f2dc92b6
Terence Chan Zun Mun <zunmun@gmail.com>
2025-02-14 23:31:16 +0800

    feat: basic sensors96b

---
+++ PYNQ/sensors96b/README.md
@@ -0,0 +1,9 @@
+Get bitstream from
+```
+sensors96b\sensors96b.runs\impl_1\sensors96b.bit
+```
+
+Get hwh file from
+```
+sensors96b\sensors96b.gen\sources_1\bd\sensors96b\hw_handoff\sensors96b.hwh
+```
---
+++ PYNQ/sensors96b/sensors96b.srcs/sources_1/new/simple.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/14/2025 02:18:46 AM
+// Design Name: 
+// Module Name: simple
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module simple(
+
+    );
+endmodule
